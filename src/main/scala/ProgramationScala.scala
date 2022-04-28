d7232630b9e7b1d9b71ed7cd2ba4030aadee29be
PLB <jlorente8@gmail.com>
2022-04-28 10:56:00 +0200

    Mon code scala

---
+++ src/main/scala/ProgramationScala.scala
@@ -0,0 +1,21 @@
+object ProgramationScala {
+
+  def main (args : Array[String]) : Unit =
+  println("Hello World, Scala")
+
+  //Definition de un methode
+  def testHello(texte:String):Unit={
+    val a = 10
+    val b = 150
+    val c = a+b
+    println("voici votre $essqge" + texte)
+    return c
+  }
+  def testHello (text:string) String={
+    val a = 10
+    val b = 150
+    val c = a+b
+    println("voici votre $essqge" + texte)
+    return c.toString
+  }
+}
